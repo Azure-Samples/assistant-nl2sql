8a31cd964161a66c37c1381134389b75644a0cb9
Luiz Braz <luiz.braz@microsoft.com>
2024-12-01 13:26:01 -0300

    Revert "Adjustments in the utils folder structure"
    This reverts commit f5df8f511ee6a1b0cb6ff362a3dad65e42ef3c7d.

--- util/import-csv-to-pgsql.py
+++ util/import-csv-to-pgsql.py
@@ -42,13 +42,14 @@ def import_csv_to_pgsql(csv_file_path, database_name, table_name):
 
 # Example usage
 if __name__ == "__main__":
-    database_name = 'virtual_assistant_db'
+    database_name = 'sample_db_vj'
     # Create database if not exists
-    create_database(database_name)
+    # create_database(database_name)
 
     # Import CSV files to PostgreSQL
     table_files = {
-        'requisitions': 'assistant-nl2sql/data/requisitions_dataset_sap_20241201.csv',
+        'table-1': './data/table1.csv',
+        'table-2': './data/table2.csv',
     }
 
     for table_name, file_path in table_files.items():
